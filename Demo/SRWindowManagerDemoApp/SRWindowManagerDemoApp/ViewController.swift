6d83d91067100e5b627c644b873deae04cb007fc
Seorenn <hirenn@gmail.com>
2015-11-23 17:05:46 +0900

    TestApp: Implement Getting Item of Outline View

--- Demo/SRWindowManagerDemoApp/SRWindowManagerDemoApp/ViewController.swift
+++ Demo/SRWindowManagerDemoApp/SRWindowManagerDemoApp/ViewController.swift
@@ -9,7 +9,7 @@
 import Cocoa
 import SRWindowManager
 
-class ViewController: NSViewController, NSOutlineViewDataSource, NSOutlineViewDelegate {
+class ViewController: NSViewController, NSOutlineViewDataSource, NSOutlineViewDelegate, NSTableViewDelegate {
     @IBOutlet weak var outlineView: NSOutlineView!
     @IBOutlet weak var statusLabel: NSTextField!
     @IBOutlet weak var openAccessbilityButton: NSButton!
@@ -102,5 +102,42 @@ class ViewController: NSViewController, NSOutlineViewDataSource, NSOutlineViewDe
             return nil
         }
     }
+    
+    @IBAction func outlineCellSelected(sender: AnyObject) {
+        guard let item = self.outlineView.itemAtRow(self.outlineView.selectedRow) else {
+            print("No Item Found")
+            return
+        }
+        
+        print("Selecting Item = \(item)")
+        
+        if item is AppItem {
+            print("This is AppItem instance. Skip.")
+        }
+        else if item is SRWindow {
+            print("This is SRWindow. Start process...")
+            let window = item as! SRWindow
+        }
+    }
+    
+    /*
+    // This may needed to call tableViewSelectionDidChange
+    func tableView(tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return true
+    }
+    
+    func tableViewSelectionDidChange(notification: NSNotification) {
+        print("Selection Did Change")
+        let column = self.outlineView.selectedColumn
+        let app = self.items[column]
+        
+        let index = self.outlineView.selectedRow
+        if index < 0 || index >= app.windows.count { return }
+        
+        let window = app.windows[index]
+        
+        print("Selecting Window: \(window)")
+    }
+    */
 }
 
